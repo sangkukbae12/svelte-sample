e06bf976d809d07e6ffbfd7e36ebe34e166b2dcf
benwizschool <ben@wizschool.io>
2021-08-29 09:14:48 +0900

    Add book component

---
+++ src/components/book.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import Button from "./button.svelte";
+  export let bookTitle;
+  export let bookPrice;
+  export let bookDescription;
+
+  const dispatch = createEventDispatcher();
+
+  function purchaseBook() {
+    dispatch("buy", bookTitle);
+  }
+</script>
+
+<div>
+  <h2>{bookTitle}</h2>
+  <p>{bookPrice}</p>
+  <p>{bookDescription}</p>
+  <Button on:click={purchaseBook}>Buy</Button>
+</div>
+
+<style>
+  div {
+    margin: 1rem;
+    padding: 1em;
+    border-radius: 4px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  }
+</style>
