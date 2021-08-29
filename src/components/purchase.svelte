26c12b322a64d8f1714086902b7b113e6e3e07d2
benwizschool <ben@wizschool.io>
2021-08-29 09:16:16 +0900

    Add purchase

---
+++ src/components/purchase.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let books;
+
+  $: total = books.reduce((acc, cur) => {
+    return (acc += cur.price);
+  }, 0);
+</script>
+
+<h2>🛒Cart</h2>
+<ul>
+  {#each books as book}
+    <li>{book.title} - {book.price}</li>
+  {/each}
+</ul>
+
+<div class="price">Total Price: {total}</div>
+
+<style>
+  ul {
+    list-style: none;
+    margin: 1rem;
+    padding: 0;
+  }
+
+  li {
+    font-size: 1.5rem;
+    margin: 1rem;
+  }
+
+  .price {
+    border-top: 1px dashed black;
+    padding-top: 1rem;
+  }
+</style>
